9eb1e1071e800f3fa907bdc00970acd6fa471975
BRUNO HENRIQUE AFONSO <44063076806@jpmorgan.sp>
2018-01-16 21:12:14 -0200

    definindo rota

--- Controllers/PrimeiraController.cs
+++ Controllers/PrimeiraController.cs
@@ -2,9 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace projetoWebServices.Controllers
 {
+    //definindo a rota para a requisição do serviço
+    [Route("api/[controller]")]
     public class PrimeiraController : Controller
     {
-        
-        
+        public IActionResult primeira()
+        {
+            return View();
+        }
     }
 }
